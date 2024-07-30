48e83bce92bcb85d8fa35a8b1c889f94dac9ef6a
equation314 <equation618@gmail.com>
2024-07-30 17:05:47 +0000

    Deploying to gh-pages from @ arceos-org/crate_interface@c999c6f3d71553de230de750386cdaa522b0a2b0 🚀

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["crate_interface",{"t":"QXX","n":["call_interface","def_interface","impl_interface"],"q":[[0,"crate_interface"]],"i":[0,0,0],"f":"```","D":"h","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["crate_interface",["",[],["lib.rs"]]]\
+]'));
+createSrcSidebar();
