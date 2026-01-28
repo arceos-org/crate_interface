e957dcd4c900e4749ad249ac8e218aab0735a362
equation314 <equation618@gmail.com>
2026-01-28 11:07:01 +0000

    Deploying to gh-pages from @ arceos-org/crate_interface@1597d91055f5102e9c075c4c39949bda2de0b9ae 🚀

---
+++ search.index/crateNames/835c0717041e.js
@@ -0,0 +1,1 @@
+rd_("ocrate_interface")
---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["crate_interface",["",[],["args.rs","def_interface.rs","errors.rs","impl_interface.rs","lib.rs","naming.rs","validator.rs"]]]]');
+//{"start":19,"fragment_lengths":[126]}
